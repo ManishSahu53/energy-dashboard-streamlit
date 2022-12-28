0b1b00f53de5efc3b41bfe665b54cb2269320ee3
“manishsahu53” <manish.sahu.civ13@iitbhu.ac.in>
2022-12-28 22:53:47 +0530

    updated config

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
+from datetime import datetime 
 from dataclasses import dataclass, field
-
 
 @dataclass
 class Config:
     """Class for keeping track of an item in inventory."""
+
+    TIMEZONE_OFFSET = 5.50  # +5:30 Indian Time # Pacific Standard Time (UTC−08:00)
+    tzinfo = datetime.timezone(datetime.timedelta(hours=TIMEZONE_OFFSET))
 
     ENERGY_TYPES = [
         'Biomass',
